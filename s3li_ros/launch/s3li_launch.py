e7c9536add34f5b209042c7f1c0fd130acbc6183
Riccardo Giubilato <riccardo.giubilato@dlr.de>
2026-03-03 17:33:25 +0100

    add tf publishers

--- s3li_ros/launch/s3li_launch.py
+++ s3li_ros/launch/s3li_launch.py
@@ -1,14 +1,33 @@
+import os 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+
+def check_arguments(context, *args, **kwargs):
+    calib_file = LaunchConfiguration('calib_file').perform(context)
+    
+    if not calib_file:
+        raise Exception("Launch error: 'calib_file' argument is mandatory but was not provided.")
+    return
+
 def generate_launch_description():
 
+    ## *.cal files are recommended, as they provide more precise stereo calibration
+    ## camera_calibration_callab_dlrrmc-07-04.cal -> capo_grillo, capo_grillo_2
+    ## camera_calibration_callab_dlrrmc-07-05.cal -> vegetation, waterfront
+    ## camera_calibration_callab_dlrrmc-07-06.cal -> straight_path, moon_lake and lava_tunnel
     calib_arg = DeclareLaunchArgument(
         'calib_file',
-        default_value='camera_calibration_callab_dlrrmc-07-04.cal',
-        description='Path to calde-style calibration file .cal'
+        description="""
+        Path to calde-style calibration file .cal (MANDATORY!)
+        Recommendations: use the following calibration files for the associated sequences listed here
+        camera_calibration_callab_dlrrmc-07-04.cal -> capo_grillo, capo_grillo_2
+        camera_calibration_callab_dlrrmc-07-05.cal -> vegetation, waterfront
+        camera_calibration_callab_dlrrmc-07-06.cal -> straight_path, moon_lake and lava_tunnel
+        """
     )
 
     calib_config = LaunchConfiguration('calib_file')
@@ -41,8 +60,52 @@ def generate_launch_description():
         output='screen'
     )
 
+    tf_left_camera_lidar = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='tf_left_camera_lidar_publisher',
+        arguments=[
+            '0.15', '-0.02', '-0.02', 
+            '-0.005', '0.0', '1.623', 'g319c_left', 'lidar']
+    )
+
+    tf_imu_camera = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='tf_left_camera_lidar_publisher',
+        arguments=[
+            '0.00749803', '0.25481334', '0.00961434',
+            '-0.50047531', '0.50210171', '-0.50009389', '0.49731723',
+            'imu', 'g319c_left']
+    )
+
+    # This relates to the "un-rectified", therefore physical, camera pair
+    tf_stereo_camera = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='stereo_link_publisher',
+        arguments=[
+            '0.08914264', '0.00009292', '-0.00204204', 
+            '-0.00183492', '0.01381102', '-0.00119056', '0.99989932', 
+            'g319c_left', 'g319c_right'
+        ]
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', os.path.join(get_package_share_directory('s3li_ros'), 'cfg', 's3li.rviz')],
+        output='screen'
+    )
+
     return LaunchDescription([
         calib_arg,
+        OpaqueFunction(function=check_arguments),
         calde_node,
-        disparity_node
+        disparity_node,
+        tf_imu_camera, 
+        tf_left_camera_lidar, 
+        tf_stereo_camera,
+        rviz_node
     ])
